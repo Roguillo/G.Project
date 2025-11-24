51f4f92af5a2bda5acba885ab2c6b78009b03097
jacubbage <jacubbage@wpi.edu>
2025-11-24 14:50:44 -0500

    Added Boundary File

--- Frontend/g.project/src/app/page.tsx
+++ Frontend/g.project/src/app/page.tsx
@@ -1,5 +1,10 @@
+'use client'                              // directive to clarify client-side. Place at top of ALL .tsx files
+import React from 'react'
 import Image from "next/image";
 import styles from "./page.module.css";
+
+import { RegisterShopper } from './boundary'
+
 
 export default function Home() {
   return (
