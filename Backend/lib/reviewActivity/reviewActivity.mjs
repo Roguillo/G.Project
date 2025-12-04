dca7d297516f714b5fe8e0ebc3c230e980abe48b
jacubbage <jacubbage@wpi.edu>
2025-12-04 15:24:43 -0500

    Working reviewActivity Code

--- Backend/lib/reviewActivity/reviewActivity.mjs
+++ Backend/lib/reviewActivity/reviewActivity.mjs
@@ -5,7 +5,8 @@ import {config} from './config.mjs'
  * event is JSON of the form:
   
   { 
-    "loginToken" : "loginToken"
+    "loginToken" : "loginToken",
+    "daysPrior": XXX
   }
   
  */
@@ -38,10 +39,10 @@ export const handler = async (event) => {
   }
 
   // Gets a list of receipts linked to a shopperID
-  let getReceipts = (shopperID) => {
+  let getReceipts = (shopperID, date) => {
     return new Promise((resolve, reject) => {
-      const selectQuery = "SELECT receiptID FROM Receipts WHERE shopperID = ?"
-      pool.query(selectQuery, [shopperID], (error, rows) => {
+      const selectQuery = "SELECT receiptID FROM Receipts WHERE shopperID = ? AND date >= ?"
+      pool.query(selectQuery, [shopperID, date], (error, rows) => {
         if (error) {
           reject(new Error("Database error: " + error.sqlMessage))
         } else {
@@ -67,13 +68,57 @@ export const handler = async (event) => {
     })
   }
 
+  // Takes desired days prior to current date and returns it in yyyy-mm-dd format
+  let convertDate = (daysPrior) => {
+    const monthData = new Array(0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334)
+    const dateObj = new Date()  // Found at https://www.w3schools.com/js/js_date_methods.asp
+    let targetDate = dateObj.getFullYear() * 365 + monthData[dateObj.getMonth()] + dateObj.getDay() - daysPrior
+
+    let returnDay
+    let returnMonth
+    let returnYear
+
+    // Calculate year
+    returnYear = Math.floor(targetDate / 365)
+    targetDate = targetDate % 365
+    if (targetDate == 0) {
+      returnYear -= 1
+      targetDate += 365
+    }
+
+    // Calculate month
+    for (let i = monthData.length-1; i >= 0; i--) {
+      if (targetDate > monthData[i]) {
+        returnMonth = i+1
+        break
+      }
+    }
+    targetDate = targetDate - monthData[returnMonth-1]
+
+    // Calculate day
+    returnDay = targetDate
+
+    // Ensures yyyy-mm-dd format (e.g. 2025-3-1 => 2025-03-01)
+    if (returnMonth < 10) {
+      returnMonth = "0" + returnMonth
+    }
+    if (returnDay < 10) {
+      returnDay = "0" + returnDay
+    }
+
+    const formattedDate = returnYear + "-" + returnMonth + "-" + returnDay
+
+    return formattedDate
+  }
 
   try {
     const token = event.loginToken
 
     const shopperID = await verifyToken(token)
 
-    const receipt = await getReceipts(shopperID)
+    const date = convertDate(event.daysPrior)
+
+    const receipt = await getReceipts(shopperID, date)
 
     // Uses the list of receipts and formats each receiptID to have a list of items it contains
     const receiptData = await Promise.all(
