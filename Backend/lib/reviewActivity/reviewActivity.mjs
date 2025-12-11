2d606fd61c93ed7389bb6266300f3353ca34e8b8
Glisso <jacubb5@outlook.com>
2025-12-11 18:31:31 -0500

    Fixed reviewActivity

--- Backend/lib/reviewActivity/reviewActivity.mjs
+++ Backend/lib/reviewActivity/reviewActivity.mjs
@@ -6,7 +6,7 @@ import {config} from './config.mjs'
   
   { 
     "loginToken" : "loginToken",
-    "daysPrior": XXX
+    "activityPeriod": "activityPeriod"
   }
   
  */
@@ -39,10 +39,10 @@ export const handler = async (event) => {
   }
 
   // Gets a list of receipts linked to a shopperID
-  let getReceipts = (shopperID, date) => {
+  let getReceiptsByPeriod = (shopperID, startDate, endDate) => {
     return new Promise((resolve, reject) => {
-      const selectQuery = "SELECT receiptID, chainID, storeID, date FROM Receipts WHERE shopperID = ? AND date >= ? ORDER BY date DESC"
-      pool.query(selectQuery, [shopperID, date], (error, rows) => {
+      const selectQuery = "SELECT receipt.receiptID, receipt.date, chain.name, chain.chainID, store.name, store.storeID FROM Receipts as receipt INNER JOIN Stores as store INNER JOIN Chains as chain ON receipt.storeID = store.storeID AND receipt.chainID = chain.chainID WHERE shopperID = ? AND date >= ? AND date <= ? ORDER BY date DESC"
+      pool.query(selectQuery, [shopperID, startDate, endDate], (error, rows) => {
         if (error) {
           reject(new Error("Database error: " + error.sqlMessage))
         } else {
@@ -53,9 +53,9 @@ export const handler = async (event) => {
   }
 
   // Gets a list of items linked to a receiptID
-  let getItem = (receipt) => {
+  let getItems = (receipt) => {
     return new Promise((resolve, reject) => {
-      const selectQuery = "SELECT itemID, name, category, price FROM Items WHERE receiptID = ?"
+      const selectQuery = "SELECT item.name, item.category, item.price, item.itemID FROM Items AS item INNER JOIN Receipts AS receipt ON receipt.receiptID = item.receiptID WHERE receipt.receiptID = ? ORDER BY item.name DESC"
       pool.query(selectQuery, [receipt], (error, rows) => {
         if (error) {
           reject(new Error("Database error: " + error.sqlMessage))
@@ -68,72 +68,95 @@ export const handler = async (event) => {
     })
   }
 
-  // Takes desired days prior to current date and returns it in yyyy-mm-dd format
-  let convertDate = (daysPrior) => {
-    const monthData = new Array(0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334)
-    const dateObj = new Date()  // Found at https://www.w3schools.com/js/js_date_methods.asp
-    let targetDate = dateObj.getFullYear() * 365 + monthData[dateObj.getMonth()] + dateObj.getDay() - daysPrior
-
-    let returnDay
-    let returnMonth
-    let returnYear
-
-    // Calculate year
-    returnYear = Math.floor(targetDate / 365)
-    targetDate = targetDate % 365
-    if (targetDate == 0) {
-      returnYear -= 1
-      targetDate += 365
-    }
-
-    // Calculate month
-    for (let i = monthData.length-1; i >= 0; i--) {
-      if (targetDate > monthData[i]) {
-        returnMonth = i+1
-        break
-      }
-    }
-    targetDate = targetDate - monthData[returnMonth-1]
-
-    // Calculate day
-    returnDay = targetDate
-
-    // Ensures yyyy-mm-dd format (e.g. 2025-3-1 => 2025-03-01)
-    if (returnMonth < 10) {
-      returnMonth = "0" + returnMonth
-    }
-    if (returnDay < 10) {
-      returnDay = "0" + returnDay
-    }
-
-    const formattedDate = returnYear + "-" + returnMonth + "-" + returnDay
-
-    return formattedDate
-  }
 
   try {
     const token = event.loginToken
 
     const shopperID = await verifyToken(token)
 
-    const date = convertDate(event.daysPrior)
+    // Gets current date and formats it without hours, minutes, etc.
+    var currDate = new Date()
+    currDate.setUTCHours(0, 0, 0, 0)
 
-    const receipt = await getReceipts(shopperID, date)
+    // Creates different start and end date 
+    var startEndDates = []
+    if (event.activityPeriod == "daily") {
+      for (let i = 1; i <= 7; i++) {
+        let tempDate = new Date(currDate)
+        tempDate.setDate(tempDate.getDate() - i + 1)
 
-    // Uses the list of receipts and formats each receiptID to have a list of items it contains
-    const receiptData = await Promise.all(
-      receipt.map(async (receipt) => {
-        const item = await getItem(receipt.receiptID)
+        startEndDates.push([tempDate, tempDate])
+      }
+
+    } else if (event.activityPeriod == "weekly") {
+      for (let i = 1; i <= 5; i++) {
+        let tempStartDate = new Date(currDate)
+        tempStartDate.setDate(tempStartDate.getDate() - i*7 + 1)
+
+        let tempEndDate = new Date(tempStartDate)
+        tempEndDate.setDate(tempEndDate.getDate() + 6)
+
+        startEndDates.push([tempStartDate, tempEndDate])
+      }
+
+    } else if (event.activityPeriod == "monthly") {
+      for (let i = 1; i <= 3; i++) {
+        let tempStartDate = new Date(currDate)
+        tempStartDate.setDate(tempStartDate.getDate() - i*30 + 1)
+
+        let tempEndDate = new Date(tempStartDate)
+        tempEndDate.setDate(tempEndDate.getDate() + 29)
+
+        startEndDates.push([tempStartDate, tempEndDate])
+      }
+
+    } else {
+      throw new Error("Invalid activity period")
+    }
+
+    // Gets a set of receipts by timeframe
+    const receiptsByPeriod = await Promise.all(
+      startEndDates.map(async (dates) => {
+        const [startDate, endDate] = dates
+        const receipts = await getReceiptsByPeriod(shopperID, startDate, endDate)
         return {
-          receiptID: receipt.receiptID, date: receipt.date, chainID: receipt.chainID, storeID: receipt.storeID,
-          items: item
+          startDate: startDate,
+          endDate: endDate,
+          // receipt.receiptID, receipt.date, chain.name, chain.chainID, store.name, store.storeID
+          receipts: receipts
         }
       })
     )
 
+    // Takes receipt sets and maps items to them
+    const receiptItemData = await Promise.all(
+      receiptsByPeriod.map(async (receipts) => {
+        const receiptsItems = await Promise.all(
+          receipts.receipts.map(async (receipt) => {
+            const items = await getItems(receipt.receiptID)
+            return {
+              receiptID: receipt.receiptID,
+              date: receipt.date,
+              chainName: receipt.name,
+              chainID: receipt.chainID,
+              storeName: receipt.name,
+              storeID: receipt.storeID,
+              items: items
+            }
+          })
+        )
+        return {
+          startDate: receipts.startDate,
+          endDate: receipts.endDate,
+          receipts: receiptsItems
+        }
+      })
+    )
+    
+
     code = 200
     result = {
-      receiptData: receiptData
+      receiptData: receiptItemData
     }
 
   } catch (error){
