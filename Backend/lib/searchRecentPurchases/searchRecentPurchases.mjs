5c12ee899e9d7c2e5fefe17c7b0738ea1e95fb07
jacubbage <jacubbage@wpi.edu>
2025-12-11 14:08:01 -0500

    Fixed searchRecentPurchases scope

--- Backend/lib/searchRecentPurchases/searchRecentPurchases.mjs
+++ Backend/lib/searchRecentPurchases/searchRecentPurchases.mjs
@@ -39,9 +39,10 @@ export const handler = async (event) => {
   }
 
   let getMatchingItems = (shopperID, searchField) => {
+    searchField = "%" + searchField + "%" // Makes the query search for items that contain the search field
     return new Promise((resolve, reject) => {
       // Found at https://www.w3schools.com/mysql/mysql_join_inner.asp
-      const selectQuery = "SELECT item.itemID, item.name, item.category, item.price, receipt.date FROM Items AS item INNER JOIN Receipts AS receipt ON receipt.receiptID = item.receiptID WHERE receipt.shopperID = ? AND item.name = ? ORDER BY date DESC";
+      const selectQuery = "SELECT item.itemID, item.name, item.category, item.price, receipt.date FROM Items AS item INNER JOIN Receipts AS receipt ON receipt.receiptID = item.receiptID WHERE receipt.shopperID = ? AND item.name LIKE ? ORDER BY date DESC";
       pool.query(selectQuery, [shopperID, searchField], (error, rows) => {
         if (error) {
           reject(new Error("Database error: " + error.sqlMessage))
