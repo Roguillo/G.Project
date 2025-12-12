cd724fbac39de1c23eb7b9ec53d1370c48599f60
Glisso <jacubb5@outlook.com>
2025-12-11 19:58:21 -0500

    Working Review Activity

--- Frontend/g.project/src/boundary/shopper/shopperHistoryView.tsx
+++ Frontend/g.project/src/boundary/shopper/shopperHistoryView.tsx
@@ -27,15 +27,16 @@ function ReviewHistory({model, instance, andRefreshDisplay}: {model: any, instan
     return (
         <div>
           <h2>Review History</h2>
-          <button onClick={() => {reviewHistory()}}>Review History</button><br></br>
+          <button onClick={() => {reviewHistory()}}>Review History</button>
           <ul>
             {receiptData.map((receipt: any) => (
-              <li key={receipt.receiptID}>{"Date: " + receipt.date.slice(0, 10) + "  | Chain: " + receipt.chainID + "  | Store: " + receipt.storeID}
+              <li key={receipt.receiptID}>
+                <b>{"Date: " + receipt.date.slice(0, 10) + "  | Chain: " + receipt.chainID + "  | Store: " + receipt.storeID}</b>
                 <ul>
                     {receipt.items.map((item: any) => (
                         <li key={item.itemID}>{"Item: " + item.name + "  | Category: " + item.category + "  | Price: " + item.price}</li>
                     ))}
-                </ul>
+                </ul><br/>
               </li>
             ))}
           </ul>
@@ -49,21 +50,13 @@ function ReviewActivity({model, instance, andRefreshDisplay}: {model: any, insta
     const [apiMessage, changeApiMessage] = React.useState<string>()
     const [receiptData, changeReceiptData] = React.useState([])
 
-
     function reviewActivity() {
-
-        const inputDaysPrior = document.getElementById("days-prior") as HTMLInputElement
-
-        const daysPrior = parseInt(inputDaysPrior.value)
-
-        if (daysPrior < 0) {
-            changeApiMessage("Enter a value >= 0")
-            return
-        }
+        const inputActivityPeriod = document.getElementById("activity-period") as HTMLInputElement
+        const activityPeriod = inputActivityPeriod.value
 
         instance.post('/reviewActivity', {
                 "loginToken": model.getLoginToken(),
-                "daysPrior": daysPrior
+                "activityPeriod": activityPeriod
             })
             .then((response: any) => {
                 let message = JSON.parse(response.data.body)  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
@@ -86,21 +79,39 @@ function ReviewActivity({model, instance, andRefreshDisplay}: {model: any, insta
 
     return (
         <div>
-          <h2>Review Activity</h2>
-          <b>Days Prior: </b><input id="days-prior" placeholder="Days Prior" data-testid="days-prior"></input>
-          <button onClick={() => {reviewActivity()}}>Review Activity</button><br></br>
-          {apiMessage}<br></br>
-          <ul>
-            {receiptData.map((receipt: any) => (
-              <li key={receipt.receiptID}>{"Receipt from " + receipt.date.slice(0, 10) + " at chain \"" + receipt.chainID + "\" and store \"" + receipt.storeID + "\""}
-                <ul>
-                    {receipt.items.map((item: any) => (
-                        <li key={item.itemID}>{"Item: " + item.name + "  | Category: " + item.category + "  | Price: " + item.price}</li>
-                    ))}
-                </ul><br />
-              </li>
-            ))}
-          </ul>
+            <h2>Review Activity</h2>
+            {/* <b>Days Prior: </b><input id="days-prior" placeholder="Days Prior" data-testid="days-prior"></input> */}
+            <select name="activity-period" id="activity-period">
+                <option value="daily">Daily</option>
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+            </select>
+
+            <button onClick={() => {reviewActivity()}}>Review Activity</button><br/>
+            {apiMessage}<br></br>
+
+            <ul>
+                {receiptData.map((receiptData: any) => (
+                <li key={receiptData.startDate}>
+                    <h3>{"From " + receiptData.startDate.slice(0, 10) + " to " + receiptData.endDate.slice(0, 10)}</h3>
+                    <b>{receiptData.receipts.length == 0 && "No receipts in this timeframe"}</b>
+                    <ul>
+                        {receiptData.receipts.map((receipt: any) => (
+                        <li key={receipt.receiptID}>
+                            <b>{"Date: " + receipt.date.slice(0, 10) + "  | Chain: " + receipt.chainName + "  | Store: " + receipt.storeName}</b>
+                            <ul>
+                                {receipt.items.map((item: any) => (
+                                    <li key={item.itemID}>
+                                        {"Item: " + item.name + "  | Category: " + item.category + "  | Price: " + item.price}
+                                    </li>
+                                ))}
+                            </ul><br/>
+                        </li>
+                        ))}
+                    </ul><br/>
+                </li>
+                ))}
+            </ul>
         </div>
     )
 }
